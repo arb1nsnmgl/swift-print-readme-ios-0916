15b962d2a880a25a737c0f99654836f285114cc5
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-08-29 14:41:57 -0400

    Complete problems

---
+++ Print.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+var favoriteWord = "Juggernaut"
+var favoriteColor = "Black"
+var favoriteMusician = "Gloc-9"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var sampleSentence = "My favorite Dota2 hero is \(favoriteWord)."
+print(sampleSentence)
+
